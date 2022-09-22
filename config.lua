bbe694cf9d425b63449393ab86ac468cf1a3569d
MoneSuper <mome9132@gmail.com>
2022-09-22 20:44:41 +0300

    Red

---
+++ config.lua
@@ -0,0 +1,46 @@
+QB = {}
+
+QB.Spawns = {
+    -- ["legion"] = {
+    --     coords = vector4(195.17, -933.77, 29.7, 144.5),
+    --     location = "legion",
+    --     label = "Legion Square",
+    -- },
+
+    -- ["policedp"] = {
+    --     coords = vector4(428.23, -984.28, 29.76, 3.5),
+    --     location = "policedp",
+    --     label = "Police Department",
+    -- },
+
+    ["elgin-ave"] = {
+        coords = vector4(394.14, -364.96, 47.49, 201.07),
+        location = "elgin-ave",
+        label = "Elgin Avenue",
+    },
+
+    ["bus-station"] = {
+        coords = vector4(452.03, -657.56, 29.09, 221.7),
+        location = "bus-station",
+        label = "Bus Station",
+    },
+
+    ["paleto"] = {
+        coords = vector4(80.57, 6424.32, 32.64, 44.98),
+        location = "paleto",
+        label = "Paleto Bay",
+    },
+
+    ["sandy-shores"] = {
+        coords = vector4(2010.4, 3784.98, 33.18, 321.07),
+        location = "sandy-shores",
+        label = "Sandy Shores",
+    },
+
+    ["mirror-park"] = {
+        coords = vector4(1172.01, -426.07, 68.35, 268.09),
+        location = "mirror-park",
+        label = "Mirror Park",
+    },
+
+}
